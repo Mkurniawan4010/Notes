35de9043a6b1c4ba8ac8850ce2488f433673c1ba
Mkurniawan4010 <130964418+Mkurniawan4010@users.noreply.github.com>
2024-04-30 09:16:48 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:notes/firebase_options.dart';
 import 'package:notes/screens/note_list_screen.dart';
+import 'package:notes/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -20,6 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Notes',
       theme: ThemeData(
+        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -15,71 +15,74 @@ class _NoteListScreenState extends State<NoteListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Notes')),
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
       body: const NoteList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text('Add'),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 10),
-                        child: Text(
-                          'Title',
-                          textAlign: TextAlign.start,
-                        ),
-                      ),
-                      TextField(
-                        controller: _titleController,
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 10),
-                        child: Text(
-                          'Description',
-                          textAlign: TextAlign.start,
-                        ),
-                      ),
-                      TextField(
-                        controller: _descriptionController,
-                      )
-                    ],
-                  ),
-                  actions: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Cancel'),
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Add'),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Text(
+                        'Title: ',
+                        textAlign: TextAlign.start,
                       ),
                     ),
-                    ElevatedButton(
-                        onPressed: () {
-                          Map<String, dynamic> notes = {};
-                          notes['title'] = _titleController.text;
-                          notes['description'] = _descriptionController.text;
-
-                          FirebaseFirestore.instance
-                              .collection('notes')
-                              .add(notes)
-                              .whenComplete(() {
-                            _titleController.clear();
-                            _descriptionController.clear();
-                            Navigator.of(context).pop();
-                          });
-                        },
-                        child: const Text('Save'))
+                    TextField(
+                      controller: _titleController,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Text(
+                        'Description: ',
+                      ),
+                    ),
+                    TextField(
+                      controller: _descriptionController,
+                    ),
                   ],
-                );
-              });
+                ),
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Map<String, dynamic> newNote = {};
+                      newNote['title'] = _titleController.text;
+                      newNote['description'] = _descriptionController.text;
+
+                      FirebaseFirestore.instance
+                          .collection('notes')
+                          .add(newNote)
+                          .whenComplete(
+                        () {
+                          Navigator.of(context).pop();
+                        },
+                      );
+                    },
+                    child: const Text('Save'),
+                  ),
+                ],
+              );
+            },
+          );
         },
-        tooltip: 'Add Notes',
+        tooltip: 'Add Note',
         child: const Icon(Icons.add),
       ),
     );
@@ -91,6 +94,8 @@ class NoteList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController titleController = TextEditingController();
+    final TextEditingController descriptionController = TextEditingController();
     return StreamBuilder(
       stream: FirebaseFirestore.instance.collection('notes').snapshots(),
       builder: (context, snapshot) {
@@ -106,25 +111,85 @@ class NoteList extends StatelessWidget {
             return ListView(
               padding: const EdgeInsets.only(bottom: 80),
               children: snapshot.data!.docs.map((document) {
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
-                  child: Card(
-                    child: ListTile(
-                      onTap: () {},
-                      title: Text(document['title']),
-                      subtitle: Text(document['description']),
-                      trailing: InkWell(
-                        onTap: () {
-                          FirebaseFirestore.instance
-                              .collection('notes')
-                              .doc(document.id)
-                              .delete();
+                return Card(
+                  child: ListTile(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('Update'),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 10),
+                                  child: Text(
+                                    'Title: ',
+                                    textAlign: TextAlign.start,
+                                  ),
+                                ),
+                                TextField(
+                                  controller: titleController,
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(top: 20),
+                                  child: Text(
+                                    'Description: ',
+                                  ),
+                                ),
+                                TextField(
+                                  controller: descriptionController,
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('cencel'),
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Map<String, dynamic> updateNote = {};
+                                  updateNote['title'] = titleController.text;
+                                  updateNote['description'] =
+                                      descriptionController.text;
+
+                                  FirebaseFirestore.instance
+                                      .collection('notes')
+                                      .doc(document.id)
+                                      .update(updateNote)
+                                      .whenComplete(
+                                    () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  );
+                                },
+                                child: const Text('Update'),
+                              ),
+                            ],
+                          );
                         },
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          child: Icon(Icons.delete),
-                        ),
+                      );
+                    },
+                    title: Text(document['title']),
+                    subtitle: Text(document['description']),
+                    trailing: InkWell(
+                      onTap: () {
+                        FirebaseFirestore.instance
+                            .collection('notes')
+                            .doc(document.id)
+                            .delete();
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Icon(Icons.delete),
                       ),
                     ),
                   ),
