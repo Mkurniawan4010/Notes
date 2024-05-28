b80776e47f346450123b2e68df34f166e4845335
Mkurniawan4010 <130964418+Mkurniawan4010@users.noreply.github.com>
2024-05-28 09:34:38 +0700

    LOKASI

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:notes/screens/note_list_screen.dart';
 import 'package:notes/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)){
+    await FlutterConfig.loadEnvVariables();
+  }
   runApp(const MyApp());
 }
 
